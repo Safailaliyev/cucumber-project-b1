71d7ceae92584d7704518ade5ede1970c8aaed70
safailaliyev <safail.aliyev1115@gmail.com>
2023-06-25 01:41:44 -0400

    day14 work

---
+++ src/test/java/com/loop/step_definitions/WikiSearchStepDefs.java
@@ -0,0 +1,36 @@
+package com.loop.step_definitions;
+
+import com.loop.pages.WikiSearchPage;
+import com.loop.utilities.ConfigurationReader;
+import com.loop.utilities.Driver;
+import io.cucumber.java.en.*;
+import org.testng.Assert;
+
+import java.lang.module.Configuration;
+
+public class WikiSearchStepDefs {
+
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
+    @Given("User is on Wikipedia home page")
+    public void user_is_on_wikipedia_home_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("wiki.url"));
+
+    }
+    @When("User types {string} is the wiki search box")
+    public void user_types_is_the_wiki_search_box(String StevenPaulJobs) {
+        wikiSearchPage.searchBox.sendKeys("Steven Jobs");
+
+    }
+    @When("User clicks wiki search button")
+    public void user_clicks_wiki_search_button() {
+        wikiSearchPage.searchButton.click();
+
+    }
+    @Then("User sees {string} is in the {string}")
+    public void user_sees_is_in_the(String name) {
+            Assert.assertEquals(Driver.getDriver().getTitle(), name + " - Wikipedia", "Does not match");
+            Assert.assertEquals( wikiSearchPage.mainHead.getText(), name );
+            Assert.assertEquals( wikiSearchPage.imageHead.getText(), name);
+    }
+
+}
